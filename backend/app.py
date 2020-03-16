dabbba5ecffc74572fecc0c5d57772d8ed8ff9b1
Ash_H <ashraff.hatz@gmail.com>
2020-03-16 19:09:45 +0000

    add /api/getWeather endpoint

--- backend/app.py
+++ backend/app.py
@@ -19,9 +19,32 @@ def getRoute():
       }
       return jsonify(error), 400
   routes = {
-    "routes":[]
+    "routes":[] # filled with all the routes from fromParam to toParam
   }
   return jsonify(routes)
+
+@app.route('/api/getWeather')
+def getWeather():
+    longitude = request.args.get('lon')
+    latitude = request.args.get('lat')
+
+    if not longitude:
+        error = {
+            "error":"'lon' query parameter unspecified"
+        }
+        return jsonify(error), 400
+    if not latitude:
+        error = {
+            "error":"'lat' query parameter unspecified"
+        }
+        return jsonify(error), 400
+    weather = {
+        "status": "", # general weather classifcation e.g 'sunny','rainy','cloudy'
+        "temp": "36.5", # Degrees celsius
+        "windSpeed": "8", # Km/h
+        "windDirection": "S", # values range from N,S,E,W
+    }
+    return jsonify(weather)
 
 if __name__ == '__main__':
   port = int(os.environ.get("PORT",5000))
