82655b7ec0d96da08d4ddaa13aea51423bb92fac
Benjamin Henrik Robson <b.robson@se18.qmul.ac.uk>
2020-03-16 20:54:28 +0000

    add comments to routes.py

--- backend/routes.py
+++ backend/routes.py
@@ -1,3 +1,9 @@
+"""
+  This is called when the API (app.py) gets a request 
+  at /api/getRoute?from=X&to=Y. 
+  
+  It should return an array of routes from google maps. 
+"""
 def getRoute(fromLocation, toLocation):
   routes = {
     "routes":[] # filled with all the routes from fromParam to toParam
