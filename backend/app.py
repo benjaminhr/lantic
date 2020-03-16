380fe0c70dc8b6274fe7bef7386a90349e1b0d6e
benjaminhr <unoceptik@gmail.com>
2020-03-16 18:26:17 +0000

    add port as environment variable

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request 
+import os
 
 app = Flask(__name__) 
 
@@ -7,4 +8,5 @@ def query_example():
   return 'Hello'
 
 if __name__ == '__main__':
-  app.run(debug=True, port=5000)
+  port = os.environ["PORT"] or 5000
+  app.run(debug=True, port=port)
