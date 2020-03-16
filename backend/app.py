a6a748c10b47fb7bb4dbb65dfc82df4da8f44372
Ash_H <ashraff.hatz@gmail.com>
2020-03-16 18:34:43 +0000

    fix app.py port

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,13 @@
-from flask import Flask, request 
+from flask import Flask, request
 import os
 
-app = Flask(__name__) 
+app = Flask(__name__)
 
 @app.route('/')
 def query_example():
   return 'Hello'
 
 if __name__ == '__main__':
-  port = os.environ["PORT"] or 5000
+
+  port = int(os.environ.get("PORT",5000))
   app.run(debug=True, port=port)
