5efc1f01c907ae8e2e86beb286c54026558edd19
benjaminhr <unoceptik@gmail.com>
2020-03-18 21:47:18 +0000

    add cors

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import os
 import weather
 import routes
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/getRoutes')
 def getRoutes():
