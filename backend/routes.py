cecf05d18ca922e31ba2d059714c24f7f47dde6c
benjaminhr <unoceptik@gmail.com>
2020-03-28 22:27:21 +0000

    add error handling for routes request

--- backend/routes.py
+++ backend/routes.py
@@ -17,7 +17,6 @@ except:
   It should return an array of routes from google maps.
 """
 
-
 def format_query(query):
     return "+".join(query.split())
 
@@ -30,10 +29,13 @@ def get_routes(from_location, to_location):
 
     for mode in transport_modes:
         url = f"https://maps.googleapis.com/maps/api/directions/json?" \
-              f"origin={from_location}&destination={to_location}&key={MAPS_API_KEY}&mode={mode}"
+                f"origin={from_location}&destination={to_location}&key={MAPS_API_KEY}&mode={mode}"
 
         maps_request = requests.get(url)
         maps_response = maps_request.json()
+
+        if "error_message" in maps_response:
+            print("Error getting routes: " + maps_response["error_message"])
 
         if len(maps_response["routes"]) == 0:
             continue
